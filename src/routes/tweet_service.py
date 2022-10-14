a8439fdbf7cc2dc0cd941dee613d91a5ea1fb8cf
Santiago <sadavid@unicesar.edu.co>
2022-10-14 17:58:43 -0500

    add new method search followerlist.

--- src/routes/tweet_service.py
+++ src/routes/tweet_service.py
@@ -11,21 +11,18 @@ main = Blueprint('tweet_service_blueprint', __name__)
 
 
 @main.route('/')
-def get_cars():
+def get_main():
     try:
-
         return jsonify({'message': "si"})
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
-
-@main.route('/search_tweets', methods=['POST'])
-def add_car():
+## search_tweets about topic 
+@main.route('/search-tweets', methods=['POST'])
+def get_tweets():
     try:
         tweets = api.search_tweets(
             q=request.json['topic'], count=1000, lang="es")  # search_tweets
-        for tweet in tweets:
-            print(tweet.text)
         affected_rows = Tweet_model.add_tweet(tweets)
         if affected_rows == True:
             return jsonify({'message': 'se guardo con exito'})
@@ -33,3 +30,17 @@ def add_car():
             return jsonify({'message': str(ex)}), 500
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
+
+## search follower list
+@main.route('/search-follower-list', methods=['POST'])
+def get_follower_list():
+    try:
+        user = api.get_user(screen_name=request.json['user'])
+        for follower in user.followers():
+            print(follower.name)
+        return jsonify({'message': 'se encontro'})
+    except Exception as ex:
+        return jsonify({'message': str(ex)}), 500        
+
+
+## search list of followers
