809d50ed0619222ad3d370d9d96f655bb7046ea0
Santiago <sadavid@unicesar.edu.co>
2022-10-14 15:32:05 -0500

    fix error unicode mysql

--- src/routes/tweet_service.py
+++ src/routes/tweet_service.py
@@ -1,11 +1,11 @@
 from crypt import methods
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, jsonify, request
 from models.entities.tweet import Tweet
 from models.tweet_model import Tweet_model
 from utils.db import db
 from utils.tweetpy import api
 
-#models
+# models
 from models import tweet_model
 main = Blueprint('tweet_service_blueprint', __name__)
 
@@ -14,19 +14,18 @@ main = Blueprint('tweet_service_blueprint', __name__)
 def get_cars():
     try:
 
-        return jsonify({'message':"si"})
+        return jsonify({'message': "si"})
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
 
-
-@main.route('/search_tweets')
+@main.route('/search_tweets', methods=['POST'])
 def add_car():
     try:
         tweets = api.search_tweets(
-            q="f1", count=1000, lang="en")  # search_tweets
+            q=request.json['topic'], count=1000, lang="es")  # search_tweets
         for tweet in tweets:
-            print(tweet.user.name)
+            print(tweet.text)
         affected_rows = Tweet_model.add_tweet(tweets)
         if affected_rows == True:
             return jsonify({'message': 'se guardo con exito'})
