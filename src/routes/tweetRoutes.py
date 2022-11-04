b1e0d2103b38fa7d649e4f5ae6b08eab38e38ea7
Santiago <sadavid@unicesar.edu.co>
2022-11-03 19:16:35 -0500

    modify comments

--- src/routes/tweetRoutes.py
+++ src/routes/tweetRoutes.py
@@ -21,22 +21,27 @@ def get_main():
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
-# search_tweets about topic
+
+
+
 @main.route('/save-tweets', methods=['POST'])
 def save_tweets():
     try:
-        tweets_save = TweetService.save_tweets(request.json['topic'],request.json['config'])
-        return jsonify(tweets_save)  ##graficar dede donde fue tweeteado
+        # search for tweets using tweepy returning a list of tweets receiving a topic
+        # and saving it to the database
+        tweets_save = TweetService.save_tweets(
+            request.json['topic'], request.json['config'])
+        return jsonify(tweets_save)
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
 
-# search tweets for database and analytics
 @main.route('/search-tweets', methods=['GET'])
 def get_tweets_database():
     try:
+        # search tweets for database receiving a topic 
         tweets = TweetService.get_tweet_list(request.json['topic'])
-        return jsonify(tweets)  # retornar la lista.
+        return jsonify(tweets)  
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
@@ -44,29 +49,36 @@ def get_tweets_database():
 @main.route('/analytics_sentiments', methods=['GET'])
 def analytics_sentiments():
     try:
+        # search tweets for database receiving a topic
         tweets = TweetService.get_tweet_list(request.json['topic'])
+        # clean text using regular expressions
         tweets_clean_text = TweetRepository.clean_text(tweets)
-        polarity =TweetService.analytics_sentiment(tweets_clean_text)
+        # returns the sentiment using the sentiment method of the textblob library
+        polarity = TweetService.analytics_sentiment(tweets_clean_text)
         return jsonify({'message': str(polarity)})
     except Exception as ex:
         jsonify({'message': str(ex)}), 500
 
 
-# search follower list
+
 @main.route('/search-follower-list', methods=['GET'])
 def get_follower_list():
     try:
+        # search for follower using tweepy returning a list of tweets receiving a 
+        # user name and saving it to the database
         follwers = TweetService.get_follower_list(request.json['user'])
-        return jsonify({'message': str(follwers)})  # retornar la base de datos
+        return jsonify({'message': str(follwers)})  
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
 
-# search list of followers
+
 @main.route('/search-following-list', methods=['GET'])
 def get_following_list():
     try:
+        # search for following using tweepy returning a list of tweets receiving a
+        # user name and saving it to the database
         following = TweetService.get_following_list(request.json['user'])
-        return jsonify({'message': str(following)})  
+        return jsonify({'message': str(following)})
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
